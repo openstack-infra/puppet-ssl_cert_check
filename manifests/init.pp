39abe0f358c14e747bb1c3e282a515c9c4f05e50
Clark Boylan <clark.boylan@gmail.com>
2019-04-04 16:12:03 -0700

    Run ssl-cert-check out of git source
    Pull the git source for the ssl-cert-check tool so that we get new
    features like support for SNI. We keep installing the distro package as
    well to pull in any deps we need like the openssl cli toolchain.
    
    Use the source luke.
    
    Change-Id: Id08e2f67062eb4faeb9303d05d8a8c58f9f36838

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class ssl_cert_check(
   $email = 'root',
   $days = '30',
 ) {
+  # Hacky way of ensuring we have the dependencies for the script installed
   package { 'ssl-cert-check':
     ensure => present,
   }
@@ -30,9 +31,18 @@ class ssl_cert_check(
     require    => Group['certcheck'],
   }
 
+  # Pull the script straight from github so that we get support for things
+  # like SNI.
+  vcsrepo { '/opt/ssl-cert-check'
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://github.com/Matty9191/ssl-cert-check',
+  }
+
   cron { 'check ssl certificates':
     user    => 'certcheck',
-    command => "ssl-cert-check -a -q -f ${domainlist_file} -x ${days} -e ${email}",
+    command => "/opt/ssl-cert-check/ssl-cert-check -a -q -f ${domainlist_file} -x ${days} -e ${email}",
     hour    => '12',
     minute  => '04',
   }
